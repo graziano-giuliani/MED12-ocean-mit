3df48613e4ebfc5ae3e6b8198f2ab7b7890db138
Graziano Giuliani <ggiulian@ictp.it>
2025-02-15 19:09:04 +0100

    remap added and ignore bin files.

---
+++ bathy/remap_bathymetry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from cdo import *
+
+if not os.path.exists('grid_description.des'):
+    print('Please prepare first the grid.')
+    sys.exit(-1)
+
+try:
+    ifile = sys.argv[1]
+except:
+    ifile = 'Mediterranean_basin.nc'
+try:
+    ofile = sys.argv[2]
+except:
+    ofile = 'MED_MIT_BATHY.nc'
+
+cdo = cdo.Cdo( )
+
+cdo.remapdis('grid_description.des,128', input=ifile,output=ofile)
