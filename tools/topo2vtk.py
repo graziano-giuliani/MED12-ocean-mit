f531810c25f30fa7b8d78e12d6dca4b9675d33c3
Graziano Giuliani <ggiulian@ictp.it>
2025-04-15 10:04:03 +0200

    two helper scripts

---
+++ tools/topo2vtk.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import numpy as np
+from netCDF4 import Dataset
+import vtk
+import vtk.util.numpy_support as numpy_support
+
+ifile = sys.argv[1]
+vname = sys.argv[2]
+
+rcmfile = Dataset(ifile,'r')
+var = rcmfile.variables[vname][:]
+
+if vname == 'elevation':
+    var = - var
+
+npoints = np.prod(var.shape).item( )
+f = open(vname+'.vtk','w')
+f.write('# vtk DataFile Version 2.0\n')
+f.write(vname+'\n')
+f.write('ASCII\n')
+f.write('DATASET STRUCTURED_POINTS\n')
+f.write('DIMENSIONS '+ 
+        ' '.join((repr(x) for x in reversed(var.shape))) + ' 1\n')
+f.write('SPACING 0.100000 0.100000 0.500000\n')
+f.write('ORIGIN 0 0 0\n')
+f.write('POINT_DATA '+ repr(npoints) +'\n')
+f.write('SCALARS volume_scalars float 1\n')
+f.write('LOOKUP_TABLE default\n')
+for val in var.flatten( ):
+    f.write(repr(val.item( ))+'\n')
+f.close()
