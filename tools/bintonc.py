e0e33e32da462c78ad7eef82cbbdb80f9a1d2764
Graziano Giuliani <ggiulian@ictp.it>
2025-05-11 18:58:32 +0200

    MITgcm to netcdf

--- tools/bintonc.py
+++ tools/bintonc.py
@@ -5,101 +5,235 @@ import sys
 import numpy as np
 import xarray as xr
 
+start_simulation = "1979-08-01 00:00:00 UTC"
+timestep = 150
+
+names = { 'SALT'        : { 'standard_name' : 'sea_water_absolute_salinity',
+                            'long_name'     : 'Ocean salinity',
+                            'units'         : 'g kg-1',
+                            'dimensions'    : 3,
+                            'coordinates'   : 'depth lat lon',
+                          },
+          'THETA'       : { 'standard_name' : 'sea_water_potential_temperature',
+                            'long_name'     : 'Ocean potential temperature',
+                            'units'         : 'degree_C',
+                            'dimensions'    : 3,
+                            'coordinates'   : 'depth lat lon',
+                          },
+          'RHO'         : { 'standard_name' : 'sea_water_density_anomaly',
+                            'long_name'     : 'Density anomaly',
+                            'units'         : 'kg m-3',
+                            'dimensions'    : 3,
+                            'coordinates'   : 'depth lat lon',
+                          },
+          'ELEVATION'   : { 'standard_name' : 'sea_surface_elevation_anomaly',
+                            'long_name'     : 'Sea Surface Elevation Anomaly',
+                            'units'         : 'm',
+                            'dimensions'    : 2,
+                            'coordinates'   : 'lat lon',
+                          },
+          'HFLUX'       : { 'standard_name' : 'net_heat_flux',
+                            'long_name'     : 'Net Heat Flux',
+                            'units'         : 'W m-2',
+                            'dimensions'    : 2,
+                            'coordinates'   : 'lat lon',
+                          },
+          'EmPmR'       : { 'standard_name' : 'net_upward_freshwater_flux',
+                            'long_name'     : 'Net upward freshwater flux',
+                            'units'         : 'm/s',
+                            'dimensions'    : 2,
+                            'coordinates'   : 'lat lon',
+                          },
+          'EXFempmr'    : { 'standard_name' : 'net_upward_freshwater_flux',
+                            'long_name'     : 'Forcing Net upward freshwater flux',
+                            'units'         : 'm/s',
+                            'dimensions'    : 2,
+                            'coordinates'   : 'lat lon',
+                          },
+          'EXFroff'     : { 'standard_name' : 'river_freshwater_flux',
+                            'long_name'     : 'Forcing River freshwater flux',
+                            'units'         : 'm/s',
+                            'dimensions'    : 2,
+                            'coordinates'   : 'lat lon',
+                          },
+          'MLD'         : { 'standard_name' : 'depth_of_mixed_layer',
+                            'long_name'     : 'Mixed Layer Depth',
+                            'units'         : 'm',
+                            'dimensions'    : 2,
+                            'coordinates'   : 'lat lon',
+                          },
+          'SFLUX'       : { 'standard_name' : 'surface_net_salinity_flux',
+                            'long_name'     : 'Surface Net Salinity Flux',
+                            'units'         : 'g m-2 s-1',
+                            'dimensions'    : 2,
+                            'coordinates'   : 'lat lon',
+                          },
+          'SFORC_S'     : { 'standard_name' : 'surface_forcing_salinity_flux',
+                            'long_name'     : 'Surface Forcing Salinity Flux',
+                            'units'         : 'g m-2 s-1',
+                            'dimensions'    : 2,
+                            'coordinates'   : 'lat lon',
+                          },
+          'SFORC_T'     : { 'standard_name' : 'surface_forcing_energy_flux',
+                            'long_name'     : 'Surface Forcing Temperature Flux',
+                            'units'         : 'W m-2',
+                            'dimensions'    : 2,
+                            'coordinates'   : 'lat lon',
+                          },
+          'SWAVE'       : { 'standard_name' : 'surface_net_shortwave_radiation_flux',
+                            'long_name'     : 'Surface Net Shortwave Flux',
+                            'units'         : 'W m-2',
+                            'dimensions'    : 2,
+                            'coordinates'   : 'lat lon',
+                          },
+          'T_SFLUX'     : { 'standard_name' : 'surface_total_salinity_flux',
+                            'long_name'     : 'Surface Total Salinity Flux',
+                            'units'         : 'g m-2 s-1',
+                            'dimensions'    : 2,
+                            'coordinates'   : 'lat lon',
+                          },
+          'USLTMASS'    : { 'standard_name' : 'zonal_mass-weight_salinity_transport',
+                            'long_name'     : 'Zonal Mass-weight Salinity Transport',
+                            'units'         : 'g kg-1 m s-1',
+                            'dimensions'    : 3,
+                            'coordinates'   : 'lat lon',
+                          },
+          'VSLTMASS'    : { 'standard_name' : 'meridional_mass-weight_salinity_transport',
+                            'long_name'     : 'Meridional Mass-weight Salinity Transport',
+                            'units'         : 'g kg-1 m s-1',
+                            'dimensions'    : 3,
+                            'coordinates'   : 'lat lon',
+                          },
+          'UTHMASS'     : { 'standard_name' : 'zonal_mass-weight_potential_temperature_transport',
+                            'long_name'     : 'Zonal Mass-weight Potntial Temperature Transport',
+                            'units'         : 'degree_C m s-1',
+                            'dimensions'    : 3,
+                            'coordinates'   : 'lat lon',
+                          },
+          'VTHMASS'     : { 'standard_name' : 'meridional_mass-weight_potential_temperature_transport',
+                            'long_name'     : 'Meridional Mass-weight Potntial Temperature Transport',
+                            'units'         : 'degree_C m s-1',
+                            'dimensions'    : 3,
+                            'coordinates'   : 'lat lon',
+                          },
+          'UVEL'        : { 'standard_name' : 'zonal_component_of_water_velocity',
+                            'long_name'     : 'Zonal Water Velocity',
+                            'units'         : 'm s-1',
+                            'dimensions'    : 3,
+                            'coordinates'   : 'lat lon',
+                          },
+          'VVEL'        : { 'standard_name' : 'meridional_component_of_water_velocity',
+                            'long_name'     : 'Meridional Water Velocity',
+                            'units'         : 'm s-1',
+                            'dimensions'    : 3,
+                            'coordinates'   : 'lat lon',
+                          },
+          'WVEL'        : { 'standard_name' : 'vertical_component_of_water_velocity',
+                            'long_name'     : 'Vertical Water Velocity',
+                            'units'         : 'm s-1',
+                            'dimensions'    : 3,
+                            'coordinates'   : 'lat lon',
+                          },
+          'UVELMASS'    : { 'standard_name' : 'zonal_mass-weighted_component_of_water_velocity',
+                            'long_name'     : 'Zonal Mass Weighted Water Velocity',
+                            'units'         : 'm s-1',
+                            'dimensions'    : 3,
+                            'coordinates'   : 'lat lon',
+                          },
+          'VVELMASS'    : { 'standard_name' : 'meridional_mass-weighted_component_of_water_velocity',
+                            'long_name'     : 'Meridional Mass Weighted Water Velocity',
+                            'units'         : 'm s-1',
+                            'dimensions'    : 3,
+                            'coordinates'   : 'lat lon',
+                          },
+          'U_WSTRESS'   : { 'standard_name' : 'surface_zonal_wind_stress',
+                            'long_name'     : 'Surface Zonal Wind Stress',
+                            'units'         : 'N m-2',
+                            'dimensions'    : 2,
+                            'coordinates'   : 'lat lon',
+                          },
+          'V_WSTRESS'   : { 'standard_name' : 'surface_meridional_wind_stress',
+                            'long_name'     : 'Surface Meridional Wind Stress',
+                            'units'         : 'N m-2',
+                            'dimensions'    : 2,
+                            'coordinates'   : 'lat lon',
+                          },
+          }
+
+depth = np.array((0.525366611563235, 1.63218471391611, 2.85905740165649, 
+    4.22284435423749, 5.74273041043513, 7.44053341546443, 9.34104895575542, 
+    11.472435216752, 13.8666410979201, 16.5598804577976, 19.5931548848141, 
+    23.0128266283666, 26.8712421967803, 31.2274055372057, 36.1476975462358, 
+    41.706635797057, 47.9876646822709, 55.0839615396682, 63.0992386518988, 
+    72.1485142374656, 82.3588177073313, 93.8697857000613, 106.834096056691, 
+    121.417677516769, 137.79962436194, 156.171738686131, 176.737619951045, 
+    199.71122379278, 225.31482162003, 253.776311213825, 285.325857647152, 
+    320.191883783609, 358.596479331253, 400.750353996665, 446.847518727332, 
+    497.059932526908, 551.532392928262, 610.377968057859, 673.67426112225, 
+    741.46076112225, 813.737468057859, 890.464892928262, 971.565432526908, 
+    1056.92601872733, 1146.40185399667, 1239.82097933125, 1336.98938378361, 
+    1437.69635764715, 1541.71981121382, 1648.83132162003, 1758.80072379278, 
+    1871.40011995104, 1986.40723868613, 2103.60812436194, 2222.79917751677, 
+    2343.78859605669, 2466.39728570006, 2590.45931770733, 2715.82201423747, 
+    2842.3457386519, 2969.90346153967, 3098.38016468227, 3227.67213579706, 
+    3357.68619754624, 3488.33890553721, 3619.55574219678, 3751.27032662837, 
+    3883.42365488481, 4015.9633804578, 4148.84314109792, 4282.02193521675, 
+    4415.46354895576, 4549.13603341546, 4683.01123041044, 4817.06434435424))
+
 NX = 660
 NY = 368
+NZ = len(depth)
 
-delz = [ 1.05073322312647, 1.16290298157928, 1.29084239390147, 
-         1.43673151126053, 1.60304060113475, 1.79256540892385,
-         2.00846567165814, 2.25430685033500, 2.53410491200126,
-         2.85237380775375, 3.21417504627911, 3.62516844082599,
-         4.09166269600144, 4.62066398484923, 5.21992003321097,
-         5.89795646843153, 6.66410130199628, 7.52849241279829,
-         8.50206181166293, 9.59648935947070, 10.8241175802607,
-         12.1978184051993, 13.7308023080591, 15.4363606120969,
-         17.3275330782464, 19.4166955701354, 21.7150669596920,
-         24.2321407237791, 26.9750549307207, 29.9479242568679,
-         33.1511686097878, 36.5808836631247, 40.2283074321641,
-         44.0794418986593, 48.1148875626752, 52.3099400364761,
-         56.6349807662321, 61.0561694929633, 65.5364166358181,
-         70.0365833641819, 74.5168305070367, 78.9380192337679,
-         83.2630599635239, 87.4581124373248, 91.4935581013408,
-         95.3446925678359, 98.9921163368753, 102.421831390212,
-         105.625075743132, 108.597945069279, 111.340859276221,
-         113.857933040308, 116.156304429865, 118.245466921754,
-         120.136639387903, 121.842197691941, 123.375181594801,
-         124.748882419739, 125.976510640529, 127.070938188337,
-         128.044507587202, 128.908898698004, 129.675043531568,
-         130.353079966789, 130.952336015151, 131.481337303999,
-         131.947831559174, 132.358824953721, 132.720626192246,
-         133.038895087999, 133.318693149665, 133.564534328342,
-         133.780434591076, 133.969959398865, 134.136268488739 ]
+for binfile in sys.argv[1:]:
+    name = os.path.basename(os.path.splitext(binfile)[0])
+    ncfile = name + '.nc'
+    vname,vdate = name.split('.')
 
-NZ = len(delz)
+    time = float(vdate)*timestep
 
-lonfile = 'LONC.bin'
-latfile = 'LATC.bin'
+    lonfile = 'LONC.bin'
+    latfile = 'LATC.bin'
 
-lon = np.fromfile(lonfile, '>f4').reshape(NY,NX)
-lat = np.fromfile(latfile, '>f4').reshape(NY,NX)
+    lon = np.fromfile(lonfile, '>f4').reshape(NY,NX)
+    lat = np.fromfile(latfile, '>f4').reshape(NY,NX)
 
-xlon = xr.DataArray(name = "lon",
-                    data = lon, 
-                    dims = ["lon","lat"],
-                    attrs = dict(standard_name = "longitude",
-                                 units = "degrees_east"))
-xlat = xr.DataArray(name = "lat",
-                    data = lat, 
-                    dims = ["lon","lat"],
-                    attrs = dict(standard_name = "latitude",
-                                 units = "degrees_north"))
+    xlon = xr.DataArray(name = "lon",
+                        data = lon, 
+                        dims = ["lon","lat"],
+                        attrs = dict(standard_name = "longitude",
+                                     units = "degrees_east"))
+    xlat = xr.DataArray(name = "lat",
+                        data = lat, 
+                        dims = ["lon","lat"],
+                        attrs = dict(standard_name = "latitude",
+                                     units = "degrees_north"))
+    xdepth = xr.DataArray(name = "depth",
+                          data = depth, 
+                          dims = ["depth"],
+                          attrs = dict(standard_name = "depth",
+                                       units = "m"))
+    xtime = xr.DataArray(name = "time",
+                         data = np.array((time,)),
+                         dims = ["time"],
+                         attrs = dict(standard_name = "time",
+                                      units = "seconds since "+start_simulation))
 
-binfile = sys.argv[1]
-name = os.path.splitext(binfile)[0]
-ncfile = name + '.nc'
-
-da = None
-try:
-    h = np.fromfile(sys.argv[1], '>f4').reshape(NY,NX)
-    da = xr.DataArray(name = name, data = h, 
-                dims = ["lon","lat"],
-                coords = dict(lon = xlon,
-                              lat = xlat),
-                attrs = dict(standard_name = "unknown",
-                             units = "1",
-                             coordinates = "lat lon"),
-                )
+    if names[vname]['dimensions'] == 3:
+        dims = ["time","depth","lon","lat"]
+        coords = dict(lon = xlon, lat = xlat, depth = xdepth, time = xtime)
+        count = NZ*NY*NX
+        h = np.fromfile(binfile, '>f4',
+                    count = count).reshape(1,NZ,NY,NX)
+    else:
+        dims = ["time","lon","lat"]
+        coords = dict(lon = xlon, lat = xlat, time = xtime)
+        count = NY*NX
+        h = np.fromfile(binfile, '>f4',
+                    count = count).reshape(1,NY,NX)
+    da = xr.DataArray(name = vname , data=h, dims = dims, coords = coords,
+                      attrs = dict(standard_name = names[vname]['standard_name'],
+                                   long_name = names[vname]['long_name'],
+                                   units = names[vname]['units'],
+                                   coordinates = names[vname]['coordinates']),
+                     )
     da.to_netcdf(ncfile)
-except:
-    count = NZ*NY*NX
-    try:
-        h = np.fromfile(sys.argv[1], '>f4',
-                count = count).reshape(NZ,NY,NX)
-        da = xr.DataArray(name = name , data=h, 
-                    dims = ["dz","lon","lat"],
-                    coords = dict(lon = xlon,
-                                  lat = xlat,
-                                  dz = (["dz"],np.array(delz))),
-                    attrs = dict(standard_name = "unknown",
-                                 units = "1",
-                                 coordinates = "lat lon"),
-                    )
-        da.to_netcdf(ncfile)
-    except:
-        xtime = xr.DataArray(name = "time",
-                    data = np.array([0.0,]), 
-                    dims = ["time"],
-                    attrs = dict(standard_name = "time",
-                        units = "days since 1970-01-01 00:00:00"))
-        h = np.fromfile(sys.argv[1], '>f4',
-                count = count).reshape(NZ,NY,NX)
-        da = xr.DataArray(name = name , data=h, 
-                    dims = ["time","dz","lon","lat"],
-                    coords = dict(lon = xlon,
-                                  lat = xlat,
-                                  dz = (["dz"],np.array(delz)),
-                                  time = xtime),
-                    attrs = dict(standard_name = "unknown",
-                                 units = "1",
-                                 coordinates = "lat lon"),
-                    )
-        da.to_netcdf(ncfile, unlimited_dims="time")
